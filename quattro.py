3ea448e2c8efcab0d94854ef6cc025aec3a01450
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2023-03-22 13:18:11 +0000

    quarto esercizio

---
+++ quattro.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import math
+app = Flask(__name__)
+def aR(a,b):
+    return a * b
+
+def dR(a,b):
+    return math.sqrt(a**2 + b**2)
+
+@app.route('/')
+def home():
+    return render_template('quattro_1.html')
+
+@app.route('/data', methods=['POST',"GET"])
+def data():
+    base = float(request.args.get('base'))
+    altezza = float(request.args.get('altezza'))
+    calcolo=request.args.getlist('calcolo')
+    calcola_area = 'area' in calcolo
+    calcola_diagonale = 'diagonale' in calcolo
+    risultato1=""
+    risultato2=""
+
+    if calcola_area:
+        risultato1 = base * altezza
+        
+    if calcola_diagonale:
+        risultato2 = math.sqrt(base**2 + altezza**2)
+
+    return render_template('quattro_2.html', base=base, altezza=altezza, calcolo=calcolo, risultato1=risultato1, risultato2=risultato2)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
